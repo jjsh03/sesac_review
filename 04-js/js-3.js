5a7a782ba40c7aea76d0e4301234ac147d9d61c2
jjsh03 <aberrant03@gmail.com>
2023-07-27 23:55:30 +0900

    study: js-operate

---
+++ 04-js/js-3.js
@@ -0,0 +1,33 @@
+let a = 5;
+let b = 2;
+
+console.log(a + b);
+console.log(a ** b);
+console.log(a ** 3);
+
+console.log(a == b);
+console.log(a === b);
+console.log(a === 5);
+console.log('5' == a);
+console.log('5' === a);
+console.log('Hello' + 'World');
+console.log('12' + '34');
+console.log('5' - '2');
+console.log(a - b);
+
+let result1, result2;
+let num = 10;
+num2 = 10;
+
+result1 = num++;
+console.log(result1);
+console.log(num);
+console.log(result1);
+
+result2 = ++num2;
+console.log(result2);
+console.log(num);
+console.log(result2);
+
+console.log((num += 1));
+console.log((num -= 1));
